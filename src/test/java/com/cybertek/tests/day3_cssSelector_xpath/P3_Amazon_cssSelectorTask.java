16147898a92d464b9e457a468fc8e64bfd943e95
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2021-01-07 23:07:10 -0600

    Merge branch 'master' of https://github.com/Cybertek-B21/SeleniumProject2020_B21
    # Conflicts:
    #	src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P1_Xpath_CssSelector.java

--- src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P3_Amazon_cssSelectorTask.java
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P3_Amazon_cssSelectorTask.java
@@ -18,7 +18,37 @@ public class P3_Amazon_cssSelectorTask {
         // 2. Go to https://www.amazon.com
 
         driver.get("https://www.amazon.com");
+
         // 3. Enter search term (use cssSelectorto locate search box)
+
+        WebElement amazonSearchBar = driver.findElement(By.cssSelector("input[id ='twotabsearchtextbox']"));
+
+        // Second syntax for css Selector: tagName#attributeValue
+        //WebElement amazonSearchBar = driver.findElement(By.cssSelector("input#twotabsearchtextbox"));
+        // tagname[attribute = 'value']
+
+        amazonSearchBar.sendKeys("apple" + Keys.ENTER);
+
+        /*
+        How to open a search bar in Chrome:
+        Mac: CMD+F
+        WINDOWS: CNTRL + F
+         */
+
         // 4. Verify title contains search term
+
+        String expectedInTitle = "apple";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.contains(expectedInTitle)){
+            System.out.println("Title contains search key. Verification PASSED!");
+        }else{
+            System.out.println("Title does not contain search key. Verification FAILED!");
+            System.out.println("actualTitle= " +actualTitle);
+            System.out.println("expectedIntTitle = " +expectedInTitle);
+        }
+
+
+
     }
     }
