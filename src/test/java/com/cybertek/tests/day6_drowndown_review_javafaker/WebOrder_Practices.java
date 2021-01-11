808c09d115c5935dce9e72fb3a4bd4f5d2de5c78
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2021-01-11 14:22:41 -0600

    Merge branch 'master' of https://github.com/Cybertek-B21/SeleniumProject2020_B21
    # Conflicts:
    #	src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/DropdownTasks.java

---
+++ src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/WebOrder_Practices.java
@@ -0,0 +1,77 @@
+package com.cybertek.tests.day6_drowndown_review_javafaker;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class WebOrder_Practices {
+
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //1. Open browser
+        //2. Go to website:
+        //http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+        driver = WebDriverFactory.getDriver("chrome");
+
+        //2. Go to http://practice.cybertekschool.com/dropdown
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        //maximize
+        driver.manage().window().maximize();
+
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void test1_link_verifications(){
+        //3. Enter username: “Tester”
+        WebElement inputUsername = driver.findElement(By.id("ctl00_MainContent_username"));
+        inputUsername.sendKeys("Tester");
+
+        //4. Enter password: “test”
+        WebElement inputPassword = driver.findElement(By.id("ctl00_MainContent_password"));
+        inputPassword.sendKeys("test");
+
+        //5. Click to Login button
+        WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
+        loginButton.click();
+
+        //6. Print out count of all the links on landing page
+        List<WebElement> allLinks = driver.findElements(By.xpath("//body//a"));
+
+        System.out.println("Number of all links in this page: " + allLinks.size());
+
+        //7. Print out each link text on this page
+        for (WebElement each : allLinks) {
+
+            System.out.println("eachLink = "+each.getText());
+
+        }
+
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws InterruptedException {
+
+        //additional 5 seconds before closing the browser
+        Thread.sleep(5000);
+
+        driver.close();
+
+    }
+
+
+}
