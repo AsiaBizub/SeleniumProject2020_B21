e70730e9ee6db336b9bf8639134e66106c5ce5ec
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2021-01-25 19:56:22 -0600

    Update WebTablePractices.java

--- src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
+++ src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
@@ -1,23 +1,31 @@
 package com.cybertek.tests.day8_testbase_properties_driver;
 
-import com.cybertek.tests.base.TestBase;
-import com.cybertek.utilities.BrowserUtils;
-import com.cybertek.utilities.WebOrderUtils;
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class WebTablePractices extends TestBase {
+import java.util.concurrent.TimeUnit;
+
+public class WebTablePractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+
+        //2. Go to website: http://practice.cybertekschool.com/javascript_alerts
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete12%2fWebOrders%2fDefault.aspx");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
 
     @Test
-    public void verify_order_test() throws InterruptedException {
-        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete12%2fWebOrders%2fDefault.aspx");
+    public void verify_order_test(){
 
-        WebOrderUtils.loginToSmartBear(driver);
-
-        //This is re-usable method we created in BrowserUtil class to replace Thread.sleep
-        BrowserUtils.sleep(1);
-
-        //Verify "Mark Smith" is in the list
-        WebOrderUtils.verifyOrder(driver, "Mark Smith");
     }
 
 
